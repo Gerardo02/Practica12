816dffac9a5678f005d1e7f25278bb195f994837
gerardo jerez <gerardo.jerez@hotmail.com>
2022-09-28 16:28:19 -0700

    heyy

---
+++ Assets/EventListener.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EventListener : MonoBehaviour
+{
+    private Renderer renderers;
+    [SerializeField] GameObject panel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderers = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEnter()
+    {
+        renderers.material.color = Color.red;
+        panel.gameObject.SetActive(true);
+    }
+    public void OnEnxit()
+    {
+        renderers.material.color = Color.white;
+        panel.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Puerta.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+    [SerializeField] float AngleY = 90.0f;
+    private float targetValue = 0.0f;
+    private float currentValue = 0.0f;
+    private float easing = 0.05f;
+    [SerializeField] GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentValue += (targetValue - currentValue) * easing;
+        target.transform.rotation = Quaternion.identity;
+        target.transform.Rotate(0, currentValue, 0);
+    }
+
+    public void OnEnter()
+    {
+        
+        targetValue = AngleY;
+        currentValue = 0.0f;
+        
+    }
+
+    public void OnExit()
+    {
+        currentValue = AngleY;
+        targetValue = 0.0f;
+    }
+}
